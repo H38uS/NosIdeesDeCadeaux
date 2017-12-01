806b2c39179622219d6bf637b696355c9ff83763
Jordan Mosio <jordan.mosio@hotmail.fr>
2017-12-01 18:20:58 +0100

    [BugFix] Logout.

--- src/main/java/com/mosioj/servlets/LogoutServlet.java
+++ src/main/java/com/mosioj/servlets/LogoutServlet.java
@@ -1,6 +1,7 @@
 package com.mosioj.servlets;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,18 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/logout")
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.mosioj.utils.ParametersUtils;
+import com.mosioj.utils.RootingsUtils;
+
+@WebServlet("/my_logout")
 public class LogoutServlet extends HttpServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5053105238179726739L;
+	private static final Logger logger = LogManager.getLogger(LogoutServlet.class);
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		request.logout();
-		response.sendRedirect(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.debug(MessageFormat.format("Déconnexion de {0}", ParametersUtils.getUserName(request)));
+		request.getSession().invalidate();
+		RootingsUtils.redirectToPage("/public/index.jsp", request, response);
 	}
 }
