6710710cad6df7db39ffe3dc38dcefb39135ed86
Jordan Mosio <jordan.mosio@hotmail.fr>
2018-02-04 13:40:57 +0100

    [Feature] Meilleur gestion 404.

---
+++ WebContent/protected/NotFound.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<t:normal_protected>
+	<jsp:body>
+		<h2>Il semblerait que vous vous soyez perdu...</h2>
+		<p>La page n'existe plus ou n'a jamais existé.</p>
+		<p>Cliquez uniquement sur les liens visibles, et envoyez un email si cela se reproduit.</p>
+	</jsp:body>
+</t:normal_protected>
---
+++ src/main/java/com/mosioj/servlets/error/NotFound.java
@@ -0,0 +1,40 @@
+package com.mosioj.servlets.error;
+
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mosioj.servlets.IdeesCadeauxServlet;
+import com.mosioj.servlets.securitypolicy.AllAccessToPostAndGet;
+import com.mosioj.utils.ParametersUtils;
+import com.mosioj.utils.RootingsUtils;
+
+@WebServlet("/public/NotFound")
+public class NotFound extends IdeesCadeauxServlet {
+
+	private static final long serialVersionUID = 936404523785343564L;
+	private static final String VIEW_PROTECTED_URL = "/protected/NotFound.jsp";
+	private static final String VIEW_PUBLIC_URL = "/public/NotFound.jsp";
+
+	public NotFound() {
+		super(new AllAccessToPostAndGet());
+	}
+
+	@Override
+	public void ideesKDoGET(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException {
+		if (ParametersUtils.getUserName(request) != null) {
+			RootingsUtils.rootToPage(VIEW_PROTECTED_URL, request, response);
+		} else {
+			RootingsUtils.rootToPage(VIEW_PUBLIC_URL, request, response);
+		}
+	}
+
+	@Override
+	public void ideesKDoPOST(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException {
+		ideesKDoGET(request, response);
+	}
+
+}
