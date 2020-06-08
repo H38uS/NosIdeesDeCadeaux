5c812e5a9162a50a239a4fded52e1efd20fb99c0
Jordan Mosio <jordan.mosio@hotmail.fr>
2020-06-08 19:17:06 +0200

    [Technique] Refactor affichage réseau : plus besoin de liste.

--- WebApp/src/main/java/com/mosioj/ideescadeaux/webapp/servlets/controllers/relations/AfficherReseau.java
+++ WebApp/src/main/java/com/mosioj/ideescadeaux/webapp/servlets/controllers/relations/AfficherReseau.java
@@ -1,12 +1,11 @@
 package com.mosioj.ideescadeaux.webapp.servlets.controllers.relations;
 
-import com.mosioj.ideescadeaux.core.model.entities.Relation;
 import com.mosioj.ideescadeaux.core.model.entities.User;
 import com.mosioj.ideescadeaux.core.model.repositories.UserRelationRequestsRepository;
-import com.mosioj.ideescadeaux.core.model.repositories.UserRelationsRepository;
 import com.mosioj.ideescadeaux.core.model.repositories.UserRelationsSuggestionRepository;
-import com.mosioj.ideescadeaux.webapp.servlets.controllers.AbstractListes;
+import com.mosioj.ideescadeaux.webapp.servlets.rootservlet.IdeesCadeauxGetServlet;
 import com.mosioj.ideescadeaux.webapp.servlets.securitypolicy.NetworkAccess;
+import com.mosioj.ideescadeaux.webapp.utils.RootingsUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,10 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
-import java.util.List;
 
 @WebServlet("/protected/afficher_reseau")
-public class AfficherReseau extends AbstractListes<Relation, NetworkAccess> {
+public class AfficherReseau extends IdeesCadeauxGetServlet<NetworkAccess> {
 
     private static final long serialVersionUID = 9147880158497428623L;
 
@@ -25,15 +23,14 @@ public class AfficherReseau extends AbstractListes<Relation, NetworkAccess> {
     public static final String SELF_VIEW = "/protected/afficher_reseau";
     public static final String DISPATCH_URL = "/protected/afficher_reseau.jsp";
 
-    /**
-     * Class constructor.
-     */
+    /** Class constructor. */
     public AfficherReseau() {
         super(new NetworkAccess(USER_ID_PARAM));
     }
 
     @Override
-    public void ideesKDoGET(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException {
+    public void ideesKDoGET(HttpServletRequest request,
+                            HttpServletResponse response) throws ServletException, SQLException {
 
         User friend = policy.getUser();
         int userId = thisOne.id;
@@ -54,34 +51,7 @@ public class AfficherReseau extends AbstractListes<Relation, NetworkAccess> {
             session.removeAttribute("accepted");
         }
 
-        super.ideesKDoGET(request, response);
-    }
-
-    @Override
-    protected String getViewPageURL() {
-        return DISPATCH_URL;
-    }
-
-    @Override
-    protected String getCallingURL() {
-        return SELF_VIEW.substring(1);
-    }
-
-    @Override
-    protected String getSpecificParameters(HttpServletRequest req) {
-        return "&" + USER_ID_PARAM + "=" + policy.getUser().id;
-    }
-
-    @Override
-    protected int getTotalNumberOfRecords(HttpServletRequest req) {
-        return UserRelationsRepository.getRelationsCount(policy.getUser());
-    }
-
-    @Override
-    protected List<Relation> getDisplayedEntities(int firstRow, HttpServletRequest req) throws SQLException {
-        return UserRelationsRepository.getRelations(policy.getUser().id,
-                                                    firstRow,
-                                                    helper.getMaxNumberOfResults());
+        RootingsUtils.rootToPage(DISPATCH_URL, request, response);
     }
 
 }
